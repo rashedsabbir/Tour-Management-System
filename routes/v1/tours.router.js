b68f87523a9f52b034de8c2bccaec64393cdb32b
rashedsabbir <rashedulhasan1996@gmail.com>
2022-09-20 22:52:48 +0600

    trending tours added

--- routes/v1/tours.router.js
+++ routes/v1/tours.router.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 //get all tours
 const getAllTours = require("../../controller/tours/getAllTours");
+
 //Post tours
 const createTour = require("../../controller/tours/createTours");
 router.route("/").get(getAllTours).post(createTour);
 
+//get trending tour
+const getTrending = require("../../controller/tours/getTrending");
+router.route("/trending").get(getTrending);
 
 module.exports = router;
