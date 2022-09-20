dad3ec43d5e4e4d6f654978ade871c8734da1ba8
rashedsabbir <rashedulhasan1996@gmail.com>
2022-09-20 20:44:18 +0600

    tour router added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const tourstRouter = require("./routes/v1/tours.router");
+const tourstRouter = require("./routes/v1/tours.router");
 
 app.use(express.json());
 app.use(cors());
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
   res.send("Routes working successfully!");
 });
 
-// app.use("/tours", tourstRouter);
+app.use("/tours", tourstRouter);
 
 module.exports = app;
