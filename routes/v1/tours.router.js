56ed63c71fca920133c1aca366785c7b0650bb4f
rashedsabbir <rashedulhasan1996@gmail.com>
2022-09-20 22:41:45 +0600

    get all tour added

--- routes/v1/tours.router.js
+++ routes/v1/tours.router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+//get all tours
+const getAllTours = require("../../controller/tours/getAllTours");
 //Post tours
 const createTour = require("../../controller/tours/createTours");
-router.route("/").post(createTour);
+router.route("/").get(getAllTours).post(createTour);
 
 
 module.exports = router;
